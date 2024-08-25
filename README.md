32dfe71220c7e6d00c7849b0783f227e1fec9635
Katarzyna Monteiro de Carvalho <siostra4@hotmail.com>
2024-08-25 13:25:23 +0200

    inicial state

---
+++ README.md
@@ -0,0 +1,16 @@
+# azureappdjango
+
+Based on https://docs.djangoproject.com/en/5.0/intro/tutorial02/
+
+My goal is to write an web server application "Sickstory"
+
+# How to start the server
+
+cd sickstory
+python manage.py runserver
+
+# How to create/update the models
+python manage.py makemigrations sickstory
+# Served page
+
+localhost:8000
---
+++ sickstory/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Patients(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+class Sickness(models.Model):
+    patient = models.ForeignKey(Patients, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    start_date = models.DateField("start_date")
+    end_date = models.DateField("end_date", null=True, blank=True)
+    symptoms = models.CharField(max_length=200)
+
